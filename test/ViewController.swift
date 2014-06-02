43d4b69c817e501dbe90e33713a4888ef39ab19c
Oleksandr Skrypnyk <ukraine.sax@gmail.com>
2014-06-03 02:59:43 +0300

    initial swift

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  let cellIdentifier = "PlainCellIdentifier"
+
+  var elements = Dictionary<String, String>()
+
+  @IBOutlet var tableView : UITableView
                             
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    for i in 1..10 {
+      elements["Row \(i)"] = "Value \(i)"
+    }
+
+    tableView.reloadData()
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+    return elements.count
   }
 
-
+  func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!  {
+    var cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as UITableViewCell
+    cell.textLabel.text = Array(elements.keys)[indexPath.row]
+    cell.detailTextLabel.text = Array(elements.values)[indexPath.row]
+    return cell
+  }
 }
 
